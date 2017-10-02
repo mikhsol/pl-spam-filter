1a20a22fc12e63e6f1bdb8136cb6f440a693947f
Mikhail <miksolodovnikov@gmail.com>
2017-10-02 20:46:30 +0500

    Add entrance point for filter.

---
+++ filter.lisp
@@ -0,0 +1,16 @@
+(in-package :com.nwps.spam)
+
+
+(defparameter *max-ham-score* .4)
+(defparameter *min-spam-score* .6)
+
+(defun classify (text)
+  (classification (score (extract-features text))))
+
+(defun classification (score)
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t 'unsure)))
+
+
